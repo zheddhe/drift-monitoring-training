02e997017046441d9daf2c83929bb65072d50f09
zheddhe <remy.canal@live.fr>
2025-09-14 16:19:25 +0000

    setup initial sur branche dédiée pour le use case taxi

--- ui_demo_all.sh
+++
@@ -1,2 +0,0 @@
-# to Open the UI on all the demo workspace
-evidently ui --demo-projects all
--- ui_workspace.sh
+++
@@ -1,3 +0,0 @@
-# to Open the UI on the workspace : datascientest-workspace/
-evidently ui --workspace \
-    ${1:-./datascientest-workspace/}
